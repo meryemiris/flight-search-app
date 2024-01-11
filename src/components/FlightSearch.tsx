d55190bf75ed857fee2361a59ee1709a435f5636
Meryem İris <irismeryem.96@gmail.com>
2024-01-11 18:41:03 +0300

    create hande functions

--- src/components/FlightSearch.tsx
+++ src/components/FlightSearch.tsx
@@ -1,19 +1,62 @@
+import { useState } from "react";
 import styles from "./FlightSearch.module.css";
 
 const currentTime = new Date().toISOString().split("T")[0];
 
 export default function FlightSearch() {
+  const [isRoundTrip, setIsRoundTrip] = useState(false);
+
+  function handleFlightSearch(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const departureAirport = formData.get("departureAirport");
+    const arrivalAirport = formData.get("arrivalAirport");
+    const departureDate = formData.get("date");
+    const returnDate = isRoundTrip ? formData.get("date") : null;
+
+    console.log(
+      "departureAirport:",
+      departureAirport,
+      "arrivalAirport:",
+      arrivalAirport,
+      "departureDate:",
+      departureDate,
+      "returnDate:",
+      returnDate
+    );
+  }
+
+  function handleAirportSearch() {}
+
+  function handleSearchType(event: React.ChangeEvent<HTMLInputElement>) {
+    setIsRoundTrip(event.target.value === "roundTrip");
+    console.log("from radio buttons:", event.target.value);
+  }
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Find your flight</h1>
       <div className={styles.flightSearch}>
         <div className={styles.searchType}>
-          <input type="radio" name="searchType" value="oneWay" />
+          <input
+            type="radio"
+            name="searchType"
+            value="oneWay"
+            checked={!isRoundTrip}
+            onChange={handleSearchType}
+          />
           <label htmlFor="oneWay">one way</label>
-          <input type="radio" name="searchType" value="roundTrip" />
+
+          <input
+            type="radio"
+            name="searchType"
+            value="roundTrip"
+            checked={isRoundTrip}
+            onChange={handleSearchType}
+          />
           <label htmlFor="roundTrip">round trip</label>
         </div>
-        <form className={styles.searchForm}>
+        <form className={styles.searchForm} onSubmit={handleFlightSearch}>
           <div className={styles.inputGroup}>
             <label className={styles.searchLabel} htmlFor="departureAirport">
               From City/Airport
@@ -41,23 +84,25 @@ export default function FlightSearch() {
             <input
               className={styles.searchInput}
               type="date"
-              name="date"
+              name="departureDate"
               defaultValue={currentTime}
               min={currentTime}
             />
           </div>
-          <div className={styles.inputGroup}>
-            <label className={styles.searchLabel} htmlFor="date">
-              Return
-            </label>
-            <input
-              className={styles.searchInput}
-              type="date"
-              name="date"
-              min={currentTime}
-              defaultValue={currentTime}
-            />
-          </div>
+          {isRoundTrip && (
+            <div className={styles.inputGroup}>
+              <label className={styles.searchLabel} htmlFor="date">
+                Return
+              </label>
+              <input
+                className={styles.searchInput}
+                type="date"
+                name="returnDate"
+                min={currentTime}
+                defaultValue={currentTime}
+              />
+            </div>
+          )}
 
           <button className={styles.searchButton} type="submit">
             Find Flights
