0b2e3cea73178d72824df7277ca6f1104c40a85b
Meryem İris <irismeryem.96@gmail.com>
2024-01-14 01:44:22 +0300

    add loader and change bg image

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import styles from "./Loader.module.css";
+
+export default function Loader() {
+  return (
+    <div className={styles.loader}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
